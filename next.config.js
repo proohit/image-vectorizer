4b4736b90fb47d2874ee641dc16c77336120be0e
proohit <direnc99@gmail.com>
2022-04-23 20:51:44 +0200

    chore: update deps

--- next.config.js
+++
@@ -1,6 +0,0 @@
-/** @type {import('next').NextConfig} */
-const withTM = require("next-transpile-modules")(["react-dnd"]); // or whatever library giving trouble
-
-module.exports = withTM({
-  reactStrictMode: true,
-});
