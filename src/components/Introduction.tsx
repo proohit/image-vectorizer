9df41e0e952ef75353eb613f45cc34e10e17d9a2
proohit <direnc99@gmail.com>
2022-04-23 20:18:20 +0200

    feat: adjusted headers

--- src/components/Introduction.tsx
+++ src/components/Introduction.tsx
@@ -7,19 +7,19 @@ const StyledChip = styled(Chip)(({ theme }) => ({
 export const Introduction: React.FC = () => {
   return (
     <>
-      <Typography variant="h1" align="center">
+      <Typography variant="h3" sx={{ margin: 4 }} align="center">
         Image Vectorizer
       </Typography>
-      <Typography variant="h3" align="center">
+      <Typography variant="h5" align="center">
         Transform JPG/JPEG/PNG/GIF images to SVG vector graphics
       </Typography>
-      <Typography variant="h4">
+      <Typography variant="h6">
         Usage:
         <ol>
           <li>
             Select an image by clicking on
             <StyledChip color="primary" label="Upload File" sx={{ mx: 1 }} />
-            or by dragging and dropping an image file into the box.
+            or by dragging and dropping an image file into the application.
           </li>
           <li>Adjust vectorization settings (coming soon)</li>
           <li>
